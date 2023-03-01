fe5b4bf5d7bbf40e5a9d99606de3513bc7c28aa1
Jordan Gensler <jordan@mystenlabs.com>
2023-02-28 18:22:29 -0800

    Add wallet kit docs site

---
+++ sdk/wallet-adapter/site/theme.config.tsx
@@ -0,0 +1,18 @@
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <span>Sui Wallet Kit</span>,
+  project: {
+    link: "https://github.com/MystenLabs/sui/tree/main/sdk/wallet-adapter",
+  },
+  chat: {
+    link: "https://discord.com/invite/Sui",
+  },
+  docsRepositoryBase:
+    "https://github.com/MystenLabs/sui/tree/main/sdk/wallet-adapter",
+  footer: {
+    text: "Copyright © 2023, Mysten Labs, Inc.",
+  },
+};
+
+export default config;
